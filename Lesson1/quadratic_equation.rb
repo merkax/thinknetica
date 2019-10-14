4d9afdf34cf676e537073d554b6c230db13353d9
merkax <mishabigr@yandex.ru>
2019-10-15 00:29:32 +0300

    Fix quadratic_qeuation.rb

--- Lesson1/quadratic_equation.rb
+++ Lesson1/quadratic_equation.rb
@@ -7,18 +7,26 @@ b = gets.chomp.to_f
 puts "Введите третий коэффициент"
 c = gets.chomp.to_f
 
+if a == 0
+  puts "Это не квадратное уравнение"
+  return
+end
+
 d = b ** 2 - (4 * a * c )
-x_only = ( -b / (2 * a))
-x_one = (-b + Math.sqrt(d) ) / (2 * a)
-x_two = (-b - Math.sqrt(d) ) / (2 * a)
-
 if d < 0
-  puts "Корней нет"
-elsif d == 0
-  puts "Дискриминант равен: #{d}, Корень :#{x_only}"
-elsif d > 0
-  # puts "Дискриминант равен: #{d},\nпервый корень :#{x_one},\nвторой корень :#{x_two}"
-  puts format("Дискриминант равен: %.2f,\nпервый корень :%.2f,"\
-    "\nвторой корень :%.2f", d, x_one, x_two)
+  puts "Отрицательный дискриминант. Корней нет"
+  return
+end
+discriminant = Math.sqrt(d)
 
+x_only = (-b / (2 * a))
+x_one = (-b + discriminant ) / (2 * a)
+x_two = (-b - discriminant ) / (2 * a)
+
+if d == 0
+    puts "Дискриминант равен: #{d}, Корень :#{x_only}"
+elsif d > 0
+    # puts "Дискриминант равен: #{d},\nпервый корень :#{x_one},\nвторой корень :#{x_two}"
+    puts format("Дискриминант равен: %.2f,\nпервый корень :%.2f,"\
+      "\nвторой корень :%.2f", d, x_one, x_two)  
 end
