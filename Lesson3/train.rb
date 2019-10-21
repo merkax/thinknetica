6ca99cc9a77931d8ffeaa9d51511b08720e50fdc
merkax <mishabigr@yandex.ru>
2019-10-21 11:02:39 +0300

    Fix homework 3

--- Lesson3/train.rb
+++ Lesson3/train.rb
@@ -1,50 +1,3 @@
-class Station
-  attr_reader :trains, :name
-
-  def initialize(name)
-    @name = name
-    @trains = []
-  end
-
-  def add_train(train)
-    trains << train 
-  end
-
-  def show_train
-    trains.each { |train| puts train.number }
-  end
-
-  def send_train(train)
-    trains.delete(train)
-  end
-
-  def select_type(type)
-    trains.select{ |train| puts train.type == type } 
-  end
-end
-
-class Route
-  attr_reader :stations
-
-  def initialize(firs_station, last_station)
-    @firs_station = firs_station
-    @last_station = last_station
-    @stations = [@firs_station, @last_station]
-  end
-
-  def add_station(station) 
-    stations.insert(1, station)
-  end 
-
-  def delete_train(station)
-    stations.delete(station) if stations[1...-1].include?(station)
-  end
-
-  def show_station
-    stations.each { |station| puts station.name } 
-  end
-end
-
 class Train
   attr_reader   :amount, :type, :number #, :route
   attr_accessor :speed 
@@ -78,8 +31,7 @@ class Train
   def accept_route(route)
     @route = route
     @current_index = 0
-    @route.stations.first.add_train(self)
-    # 1)мб добавить в классе Route метод first_station(кот. будет возвращать 1 станцию), чтобы сократить здесь stations.first?
+    current_station.add_train(self)
   end
   
   def current_station
@@ -87,7 +39,7 @@ class Train
   end
 
   def next_station
-    @route.stations[@current_index + 1] unless @current_index == @route.stations.last 
+    @route.stations[@current_index + 1] unless @current_station == @route.stations.last 
   end
 
   def back_station
@@ -110,4 +62,5 @@ class Train
     end  
   end
 end
+
 
